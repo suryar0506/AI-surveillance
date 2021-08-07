50701835c841944113f035f6a89be794c3d47c03
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-08-07 17:37:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("I'm not empty!");
+var video = "";
+var status = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status_display").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
