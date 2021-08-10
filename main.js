58b9b1930a7ace085520e281fa94165cf2679e71
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-08-10 17:39:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var video = "";
 var status = "";
+var objects = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -11,13 +12,38 @@ function setup(){
     canvas.center();
 }
 
+function getResults(error,result){
+if (error){
+console.error(error);
+}else{
+    console.log(result);
+    objects = result;
+}
+}
+
 function draw(){
     image(video, 0, 0, 480, 380);
-}
+
+    if (status != ""){
+        objectDetector.detect(video, getResults);
+        for(var i = 0;i<objects.length;i++){
+            document.getElementById("status_display").innerHTML = "Detecting Objects";
+            document.getElementById("num_of_objects_display").innerHTML = objects.length;
+
+            fill('red');
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15 );
+            noFill();
+            stroke('red');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+    }
+
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status_display").innerHTML = "Detecting Objects";
+    
 }
 
 function modelLoaded(){
